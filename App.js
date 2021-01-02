16e2558ab6e1afff5849ce88511dd4ecfe71235a
hjKim-FireCop365 <hjkim@firecop365.com>
2021-01-02 15:46:27 +0900

    push rn forth app

---
+++ App.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import AppLoading from 'expo-app-loading'
+import * as Font from 'expo-font'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+import productsReducer from './store/reducers/products'
+import cartReducer from './store/reducers/cart'
+import orderReducer from './store/reducers/order'
+import ShopNavigator from './navigation/ShopNavigator'
+
+const rootReducer = combineReducers({
+  products: productsReducer,
+  cart: cartReducer,
+  orders: orderReducer,
+})
+
+const store = createStore(rootReducer)
+// const store = createStore(rootReducer, composeWithDevTools())
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  })
+}
+
+export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false)
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => {
+          setFontLoaded(true)
+        }}
+        onError={(err) => {
+          console.error(err)
+        }}
+      />
+    )
+  }
+  return (
+    <Provider store={store}>
+      <ShopNavigator />
+    </Provider>
+  )
+}
