1f2e548bbeeec8eaac9ba100da0698de939d1971
hjKim-FireCop365 <hjkim@firecop365.com>
2021-01-03 16:07:34 +0900

    Fix loading indicator &  error exception

--- store/actions/products.js
+++ store/actions/products.js
@@ -41,12 +41,17 @@ export const fetchProducts = () => {
 
 export const deleteProduct = (productId) => {
   return async (dispatch) => {
-    await fetch(
+    const response = await fetch(
       `https://rn-forth-app-default-rtdb.firebaseio.com/products/${productId}.json`,
       {
         method: 'DELETE',
       }
     )
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!')
+    }
+
     dispatch({ type: DELETE_PRODUCT, pid: productId })
   }
 }
@@ -86,7 +91,7 @@ export const createProduct = (title, description, imageUrl, price) => {
 
 export const updateProduct = (id, title, description, imageUrl) => {
   return async (dispatch) => {
-    await fetch(
+    const response = await fetch(
       `https://rn-forth-app-default-rtdb.firebaseio.com/products/${id}.json`,
       {
         method: 'PATCH',
@@ -100,6 +105,10 @@ export const updateProduct = (id, title, description, imageUrl) => {
         }),
       }
     )
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!')
+    }
 
     dispatch({
       type: UPDATE_PRODUCT,
